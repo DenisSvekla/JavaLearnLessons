e2470caa037eddd26042ee0baf3163577f5ee7cf
denissv <Vbycr@123>
2022-10-11 23:08:41 +0300

    вв

---
+++ src/lesson6Class_Konstructor_Metods/Calculator.java
@@ -0,0 +1,52 @@
+package lesson6Class_Konstructor_Metods;
+
+public class Calculator {
+    double weight;
+    int cost;
+    String color;
+
+    public Calculator () {
+
+    }
+
+    public Calculator(double weight, int cost, String color) {
+        this.weight = weight;
+        this.cost = cost;
+        this.color = color;
+    }
+
+    public Calculator(double weight, String color) {
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Calculator(int cost) {
+        this.cost = cost;
+    }
+    {
+        System.out.println("Создается калькулятор");
+    }
+
+    int summ (int a, int b) {
+        int c = a + b;
+        return c;
+    }
+
+    int minus (int a, int b) {
+        int c = a - b;
+        return c;
+    }
+
+    int multiplication (int a, int b) {
+        int c = a * b;
+        return c;
+    }
+
+    int division (int a, int b) {
+        int c = a / b;
+        return c;
+    }
+    void getAllInfo (){
+        System.out.println("Цвет " + color + "Вес "+ weight + "Цена " + cost);
+    }
+}
--- src/lesson6Class_Konstructor_Metods/Main.java
+++ src/lesson6Class_Konstructor_Metods/Main.java
@@ -1,6 +1,41 @@
+package lesson6Class_Konstructor_Metods;
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in)
+//        System.out.println("Запускаю метод getMyName");
+//        System.out.println(getName("Denis", 28));
+//        printMyBook("Any name");
+//        int[] masiveInt = new int[10];
+//        getFullMassive(masiveInt);
+//        System.out.println(Arrays.toString(masiveInt));
+//
+//    }
+//
+//    public static String getName(String name, int age) {
+//        return "My name is " + name + ", my age is " + age;
+//    }
+//
+//    public static void printMyBook(String bookName) {
+//        System.out.println("Plz print my book " + bookName);
+//    }
+//
+//    //Создать метод который будет на вход принимать пустой int массив
+//    // а на выходе отдавать заполненный случайными значениями
+//
+//    public static int[] getFullMassive(int[] massiveInt) {
+//        Random random = new Random();
+//        for (int i = 0; i < massiveInt.length; i++) {
+//            massiveInt[i] = random.nextInt(100);
+//        }
+//        return massiveInt;
 
-    }
+        Calculator calculator = new Calculator(1);
+        Calculator calculator1 = new Calculator(100.0,"Black");
+        calculator1.getAllInfo();
+        calculator.getAllInfo();
+
+   }
 }
