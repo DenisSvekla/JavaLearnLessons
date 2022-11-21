72a6a4d93001a8624a93881f45485c0c6e598278
denissv <Vbycr@123>
2022-11-21 18:39:26 +0300

    Домашняя Работа по ApiStream

---
+++ src/Lessons/lesson16VatAndJava8_17/Car.java
@@ -0,0 +1,8 @@
+package Lessons.lesson16VatAndJava8_17;
+
+public sealed class Car permits Main {
+
+    public void create() {
+
+    }
+}
---
+++ src/Lessons/lesson16VatAndJava8_17/Main.java
@@ -0,0 +1,23 @@
+package Lessons.lesson16VatAndJava8_17;
+
+import javax.lang.model.element.VariableElement;
+import javax.swing.*;
+import java.awt.*;
+
+public final class Main extends Car {
+
+    String[] lines = {"one","two","three"};
+    String result = String.join("|", lines);
+
+    public static void main(String[] args) {
+
+        String sss= """
+                Вфывфыв;
+                фывфыв;
+                """;
+        System.out.println(sss);
+
+
+    }
+
+}
